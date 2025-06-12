95a203fa03164bdfe96e4bd69e68687c22f97c5d
Christian Smith <christianryansmith@gmail.com>
2025-06-11 23:48:33 -0500

    feat: after commit to live update

--- app/models/chat_thread.rb
+++ app/models/chat_thread.rb
@@ -3,4 +3,6 @@ class ChatThread < ApplicationRecord
   has_many :chat_messages, dependent: :destroy
 
   validates :title, presence: true, length: { minimum: 3, maximum: 100 }
+
+  broadcasts_to ->(thread) { [thread.user, :chat_threads] }, partial: "chat_threads/thread_list", target: "chat_threads"
 end
