ca17f00d82a4256e4b5063810a6ee0e9c6307915
Christian Smith <christianryansmith@gmail.com>
2025-06-15 13:25:03 -0500

    fix: purge old user based messages

--- db/migrate/20250614214427_create_chats.rb
+++ db/migrate/20250614214427_create_chats.rb
@@ -8,6 +8,9 @@ class CreateChats < ActiveRecord::Migration[8.0]
     end
 
     remove_reference :messages, :user
+
+    Message.where(chat: nil).destroy_all
+
     add_reference :messages, :chat, null: false, foreign_key: true
   end
 end
