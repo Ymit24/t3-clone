539a37725fd7e98a11dab2304c00139291d97b54
Christian Smith <christianryansmith@gmail.com>
2025-06-16 22:31:39 -0500

    fix: issue with destroy button

--- app/components/chat_action_component.rb
+++ app/components/chat_action_component.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class ChatActionComponent < ViewComponent::Base
-  def initialize(tag: :a, icon: nil, color: nil, **other)
+  def initialize(tag: :a, href: nil, icon: nil, color: nil, **other)
     @tag = tag || :link
+    @href = href
     @icon = icon
     @color = color
     @other = other
