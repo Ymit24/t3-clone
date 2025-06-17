dedfac75050159d4797a363ab1b508e0c959a5c8
Christian Smith <christianryansmith@gmail.com>
2025-06-16 23:56:50 -0500

    feat: add back model info

--- app/components/chat_bubble_component.rb
+++ app/components/chat_bubble_component.rb
@@ -2,6 +2,7 @@
 
 class ChatBubbleComponent < ViewComponent::Base
   renders_one :body
+  renders_one :info
   renders_many :actions, types: {
     link: lambda { |**args| ChatActionComponent.new(tag: :a, **args) },
     button: lambda { |**args| ChatActionComponent.new(tag: :button, **args) }
